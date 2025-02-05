42ee1fc46c688081b341f230bccedbc6fe598171
OSSU <o.ss.u2056@gmail.com>
2025-02-05 21:50:09 +0900

    :recycle: Refactor #45: 사용하지 않는 import문 제거

--- app/src/main/java/com/umc/edison/ui/BaseContent.kt
+++ app/src/main/java/com/umc/edison/ui/BaseContent.kt
@@ -3,14 +3,12 @@ package com.umc.edison.ui
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.umc.edison.presentation.base.BaseState
 import com.umc.edison.ui.theme.White000
 
