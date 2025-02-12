6d77d9d7dbee7cb081b36a747fc583589e4de7fb
OSSU <o.ss.u2056@gmail.com>
2025-02-13 04:53:33 +0900

    :sparkles: Feature #52: 스페이스 탭 검색 아이콘 추가

--- app/src/main/java/com/umc/edison/ui/space/BubbleSpaceScreen.kt
+++ app/src/main/java/com/umc/edison/ui/space/BubbleSpaceScreen.kt
@@ -13,10 +13,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -29,10 +32,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.umc.edison.R
 import com.umc.edison.ui.label.LabelTabScreen
 import com.umc.edison.ui.theme.Gray300
 import com.umc.edison.ui.theme.Gray800
@@ -66,17 +72,31 @@ fun BubbleSpaceScreen(
             .fillMaxSize()
             .background(White000)
     ) {
-        Spacer(modifier = Modifier.height(33.dp))
+        Spacer(modifier = Modifier.height(30.dp))
 
         Row(
             modifier = Modifier
-                .size(width = 192.dp, height = 28.dp)
+                .wrapContentHeight()
+                .padding(horizontal = 24.dp)
                 .align(Alignment.CenterHorizontally),
         ) {
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_topbar_search),
+                contentDescription = "Search",
+                tint = Gray800,
+                modifier = Modifier
+                    .size(32.dp)
+                    .clickable {
+                        // TODO: 검색 기능 추가
+                    }
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
+
             tabs.forEachIndexed { index, text ->
                 Box(
                     modifier = Modifier
-                        .weight(1f)
+                        .width(192.dp / tabs.size)
                         .clip(RoundedCornerShape(100.dp))
                         .clickable(
                             indication = null,
@@ -98,8 +118,10 @@ fun BubbleSpaceScreen(
                     )
                 }
             }
-            
-            // TODO: 검색 버튼 추가하기
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Spacer(modifier = Modifier.size(32.dp))
         }
 
         Box(
