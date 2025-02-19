281d52987f25af3ba9033a3855aff55449c09450
HwangJaemin49 <ch06159@gmail.com>
2025-02-19 21:24:22 +0900

    Feature #71: 정렬 기능 추가

--- app/src/main/java/com/umc/edison/ui/artboard/ArtLetterSearchScreen.kt
+++ app/src/main/java/com/umc/edison/ui/artboard/ArtLetterSearchScreen.kt
@@ -1,6 +1,7 @@
 package com.umc.edison.ui.artboard
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
@@ -15,12 +16,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -31,18 +34,26 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Popup
+import androidx.compose.ui.window.PopupProperties
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import coil3.compose.AsyncImage
 import com.umc.edison.R
+import com.umc.edison.presentation.artletter.ArtLetterHomeViewModel
 import com.umc.edison.presentation.artletter.ArtLetterSearchState
 import com.umc.edison.presentation.artletter.ArtLetterSearchViewModel
 import com.umc.edison.presentation.model.ArtLetterPreviewModel
@@ -56,6 +67,7 @@ import com.umc.edison.ui.theme.Gray400
 import com.umc.edison.ui.theme.Gray500
 import com.umc.edison.ui.theme.Gray600
 import com.umc.edison.ui.theme.Gray800
+import com.umc.edison.ui.theme.White000
 
 @Composable
 fun ArtLetterSearchScreen(
@@ -137,17 +149,7 @@ fun ArtLetterSearchScreen(
 
                     else -> {
                         item {
-                            Row(
-                                verticalAlignment = Alignment.CenterVertically
-                            ) {
-                                ResultChip(text = "# ${lastSearchedQuery.value}")
-                                Spacer(modifier = Modifier.width(8.dp))
-                                Text(
-                                    text = "에 관련된 아트레터",
-                                    color = Gray800,
-                                    style = MaterialTheme.typography.displayMedium
-                                )
-                            }
+                            SearchResultBar(viewModel, lastSearchedQuery.value)
                         }
 
                         item { Spacer(modifier = Modifier.height(16.dp)) }
@@ -172,6 +174,138 @@ fun ArtLetterSearchScreen(
                         }
                     }
                 }
+            }
+        }
+    }
+}
+
+// 검색 완료 헤더
+@Composable
+fun SearchResultBar(
+    viewModel: ArtLetterSearchViewModel,
+    text: String
+) {
+    var showPopup by remember { mutableStateOf(false) }
+
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        ResultChip(text = "# " + text)
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(
+            text = "에 관련된 아트레터",
+            color = Gray800,
+            style = MaterialTheme.typography.displayMedium
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Box {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_filter),
+                contentDescription = "Filter",
+                modifier = Modifier
+                    .size(32.dp)
+                    .clickable { showPopup = true },
+                tint = Gray800
+            )
+
+            if (showPopup) {
+                ArtLetterSearchPopup(
+                    onLikesSort = {
+                        viewModel.searchArtLetters(text, "likes") // 좋아요 순 정렬 요청
+                        showPopup = false
+                    },
+                    onScrapsSort = {
+                        viewModel.searchArtLetters(text, "scraps") // 스크랩 순 정렬 요청
+                        showPopup = false
+                    },
+                    onRecentSort = {
+                        viewModel.searchArtLetters(text, "latest") // 최신순 정렬 요청
+                        showPopup = false
+                    },
+                    onDismiss = {
+                        showPopup = false
+                    }
+                )
+            }
+        }
+
+    }
+}
+
+@Composable
+private fun ArtLetterSearchPopup(
+    onLikesSort: () -> Unit,
+    onScrapsSort: () -> Unit,
+    onRecentSort: () -> Unit,
+    onDismiss: () -> Unit
+) {
+    Popup(
+        alignment = Alignment.TopEnd,
+        offset = IntOffset(-20, 80),
+        properties = PopupProperties(
+            dismissOnClickOutside = true,
+            focusable = true
+        ),
+        onDismissRequest = { onDismiss() }
+    ) {
+        Box(
+            modifier = Modifier
+                .wrapContentSize()
+                .clip(RoundedCornerShape(16.dp))
+                .background(
+                    brush = Brush.linearGradient(
+                        colors = listOf(Gray300, White000),
+                    )
+                )
+                .shadow(
+                    elevation = 8.dp,
+                    shape = RoundedCornerShape(16.dp),
+                    ambientColor = Color(0xFF3A3D40).copy(alpha = 0.12f),
+                    spotColor = Color(0xFF3A3D40).copy(alpha = 0.12f),
+                )
+                .padding(1.dp),
+        ) {
+            Column(
+                modifier = Modifier
+                    .width(150.dp)
+                    .wrapContentHeight()
+                    .clip(RoundedCornerShape(16.dp))
+                    .background(White000)
+                    .padding(horizontal = 16.dp, vertical = 14.dp),
+                verticalArrangement = Arrangement.spacedBy(12.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "공감 순",
+                    modifier = Modifier.clickable { onLikesSort() },
+                    style = MaterialTheme.typography.bodySmall,
+                    color = Gray800,
+                    textAlign = TextAlign.Center
+                )
+
+                HorizontalDivider(modifier = Modifier.border(1.dp, Gray300))
+
+                Text(
+                    text = "스크랩 순",
+                    modifier = Modifier.clickable { onScrapsSort() },
+                    style = MaterialTheme.typography.bodySmall,
+                    color = Gray800,
+                    textAlign = TextAlign.Center
+                )
+
+                HorizontalDivider(modifier = Modifier.border(1.dp, Gray300))
+
+                Text(
+                    text = "최신 순",
+                    modifier = Modifier.clickable { onRecentSort() },
+                    style = MaterialTheme.typography.bodySmall,
+                    color = Gray800,
+                    textAlign = TextAlign.Center
+                )
             }
         }
     }
@@ -312,7 +446,7 @@ fun Chip(text: String) {
 fun ResultChip(text: String) {
     Box(
         modifier = Modifier
-            .background(Gray300.copy(alpha = 0.2f), shape = RoundedCornerShape(10.dp))
+            .background(Gray300, shape = RoundedCornerShape(10.dp))
             .padding(horizontal = 12.dp, vertical = 8.dp)
     ) {
         Text(text = text,
