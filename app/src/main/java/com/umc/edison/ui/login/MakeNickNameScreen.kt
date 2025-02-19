74411bd7b9f53aa1e23556946c9f32f40a40365b
OSSU <o.ss.u2056@gmail.com>
2025-02-20 04:33:52 +0900

    :bug: Fix #76: 회원가입 닉네임 설정 엔터 방지 추가

--- app/src/main/java/com/umc/edison/ui/login/MakeNickNameScreen.kt
+++ app/src/main/java/com/umc/edison/ui/login/MakeNickNameScreen.kt
@@ -2,11 +2,9 @@ package com.umc.edison.ui.login
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
@@ -21,10 +19,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.umc.edison.presentation.login.MakeNickNameViewModel
@@ -58,54 +55,40 @@ fun MakeNickNameScreen(
                 .fillMaxWidth()
                 .padding(24.dp),
             verticalArrangement = Arrangement.Top,
-
-            ) {
-
+        ) {
 
             Text(
                 text = "에디슨에서 사용할\n닉네임을 설정해주세요.",
                 color = Gray800,
                 style = MaterialTheme.typography.displayLarge,
-                modifier = Modifier.padding(top=67.dp, bottom=24.dp)
+                modifier = Modifier.padding(top = 67.dp, bottom = 24.dp)
             )
 
-            Box(
+            TextField(
+                value = textState,
+                onValueChange = { if (it.length <= 20) textState = it },
                 modifier = Modifier
                     .fillMaxWidth()
-                    .background(Gray100, shape = RoundedCornerShape(16.dp))
-                    .height(48.dp)
-
-            ) {
-                TextField(
-                    value = textState,
-                    onValueChange = {
-                        if (it.length <= 20) {
-                            textState = it
-                        }
-                    },
-                    shape = RoundedCornerShape(16.dp),
-                    textStyle = TextStyle(fontSize = 16.sp, color = Gray800),
-                    placeholder = {
-                        Text(
-                            text = "닉네임을 입력해주세요. (최대 20자)",
-                            style = MaterialTheme.typography.titleMedium,
-                            color = Gray600
-                        )
-                    },
-                    colors = TextFieldDefaults.colors(
-                        focusedContainerColor = Gray100,
-                        unfocusedContainerColor = Gray100,
-                        disabledContainerColor = Gray100,
-                        focusedIndicatorColor = Color.Transparent,
-                        unfocusedIndicatorColor = Color.Transparent,
-                        disabledIndicatorColor = Color.Transparent
-                    ),
-
-                    modifier = Modifier
-                        .fillMaxWidth()
+                    .clip(RoundedCornerShape(16.dp))
+                    .background(Gray100),
+                placeholder = {
+                    Text(
+                        text = textState.ifEmpty { "닉네임을 입력해주세요. (최대 20자)" },
+                        style = MaterialTheme.typography.titleMedium,
+                        color = Gray600,
+                    )
+                },
+                textStyle = MaterialTheme.typography.bodyMedium,
+                singleLine = true,
+                colors = TextFieldDefaults.colors(
+                    unfocusedContainerColor = Color.Transparent,
+                    focusedContainerColor = Color.Transparent,
+                    disabledContainerColor = Color.Transparent,
+                    cursorColor = Gray800,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent
                 )
-
-            }
+            )
 
             Spacer(modifier = Modifier.weight(1f))
 
@@ -117,13 +100,8 @@ fun MakeNickNameScreen(
                     viewModel.makeNickName(textState, navHostController)
                 },
             )
-
         }
-
-
     }
-
-
 }
 
 
