05f014823ba456941d4d06ded293833bb12a89c2
OSSU <o.ss.u2056@gmail.com>
2025-02-20 04:56:50 +0900

    :sparkles: Feature #76: 아트레터 화면 스크랩 및 좋아요 기능 비로그인 상태에서 클릭 시 로그인 모달창 띄우기

--- app/src/main/java/com/umc/edison/presentation/artletter/ArtLetterDetailState.kt
+++ app/src/main/java/com/umc/edison/presentation/artletter/ArtLetterDetailState.kt
@@ -6,11 +6,12 @@ import com.umc.edison.presentation.model.ArtLetterPreviewModel
 
 data class ArtLetterDetailState(
     override val isLoading: Boolean = false,
-    val isLoggedIn: Boolean = false,
     override val error: Throwable? = null,
     override val toastMessage: String? = null,
     val artLetter: ArtLetterDetailModel,
     val relatedArtLetters: List<ArtLetterPreviewModel> = emptyList(),
+    val isLoggedIn: Boolean = false,
+    val showLoginModal: Boolean = false,
 ) : BaseState {
     companion object {
         val DEFAULT = ArtLetterDetailState(
