971dc6b121c3897725e3308d9f7dad1c7e77ac57
OSSU <o.ss.u2056@gmail.com>
2025-02-21 02:41:38 +0900

    :bug: HOTFIX: 버블 복원 이후 동기화 되도록 수정

--- app/src/main/java/com/umc/edison/presentation/mypage/TrashViewModel.kt
+++ app/src/main/java/com/umc/edison/presentation/mypage/TrashViewModel.kt
@@ -1,8 +1,10 @@
 package com.umc.edison.presentation.mypage
 
+import androidx.lifecycle.viewModelScope
 import com.umc.edison.domain.usecase.bubble.DeleteBubblesUseCase
 import com.umc.edison.domain.usecase.mypage.GetTrashedBubblesUseCase
 import com.umc.edison.domain.usecase.mypage.RecoverBubblesUseCase
+import com.umc.edison.domain.usecase.sync.SyncDataUseCase
 import com.umc.edison.presentation.base.BaseViewModel
 import com.umc.edison.presentation.model.BubbleModel
 import com.umc.edison.presentation.model.toPresentation
@@ -10,13 +12,15 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class TrashViewModel @Inject constructor(
     private val getTrashedBubblesUseCase: GetTrashedBubblesUseCase,
     private val recoverBubblesUseCase: RecoverBubblesUseCase,
-    private val deleteBubblesUseCase: DeleteBubblesUseCase
+    private val deleteBubblesUseCase: DeleteBubblesUseCase,
+    private val syncDataUseCase: SyncDataUseCase
 ) : BaseViewModel() {
     private val _uiState = MutableStateFlow(TrashState.DEFAULT)
     val uiState = _uiState.asStateFlow()
@@ -109,6 +113,7 @@ class TrashViewModel @Inject constructor(
                         bubbles = it.bubbles - it.selectedBubbles.toSet(),
                     )
                 }
+                syncData()
             },
             onError = { error ->
                 _uiState.update { it.copy(error = error) }
@@ -120,6 +125,16 @@ class TrashViewModel @Inject constructor(
                 _uiState.update { it.copy(isLoading = false) }
             }
         )
+    }
+
+    private fun syncData() {
+        viewModelScope.launch {
+            try {
+                syncDataUseCase()
+            } catch (e: Throwable) {
+                _uiState.update { it.copy(error = e) }
+            }
+        }
     }
 
     override fun clearToastMessage() {
