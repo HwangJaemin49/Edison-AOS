bcf8d5a03d45d9e5905ff0f5000bd86c65502cfb
SuHyeon00 <o.ss.u2056@gmail.com>
2025-05-11 15:46:24 +0900

    :recycle: Refactor(#86): 도메인 레이어 최근 검색어 관련 유즈케이스 네이밍에 맞춰 수정

---
+++ app/src/main/java/com/umc/edison/domain/usecase/recentSearch/DeleteRecentSearchUseCase.kt
@@ -0,0 +1,13 @@
+package com.umc.edison.domain.usecase.recentSearch
+
+import com.umc.edison.data.DataResource
+import com.umc.edison.domain.repository.ArtLetterRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class DeleteRecentSearchUseCase @Inject constructor(
+    private val artLetterRepository: ArtLetterRepository
+) {
+    operator fun invoke(keyword: String): Flow<DataResource<Unit>> =
+        artLetterRepository.removeRecentSearch(keyword)
+}
