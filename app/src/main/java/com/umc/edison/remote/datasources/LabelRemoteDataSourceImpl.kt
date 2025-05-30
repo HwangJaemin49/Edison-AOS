71b33500c11f9bf6e5b0cf49d73e70eb35eeb3f6
SuHyeon00 <o.ss.u2056@gmail.com>
2025-05-30 13:01:09 +0900

    :sparkles: Feature(#86): 라벨 동기화 서버 api 연결

--- app/src/main/java/com/umc/edison/remote/datasources/LabelRemoteDataSourceImpl.kt
+++ app/src/main/java/com/umc/edison/remote/datasources/LabelRemoteDataSourceImpl.kt
@@ -2,9 +2,12 @@ package com.umc.edison.remote.datasources
 
 import com.umc.edison.data.datasources.LabelRemoteDataSource
 import com.umc.edison.data.model.label.LabelEntity
+import com.umc.edison.remote.api.SyncApiService
+import com.umc.edison.remote.model.sync.toSyncLabelRequest
 import javax.inject.Inject
 
 class LabelRemoteDataSourceImpl @Inject constructor(
+    private val syncApiService: SyncApiService,
 ) : LabelRemoteDataSource{
     override suspend fun addLabel(label: LabelEntity): LabelEntity {
         TODO("Not yet implemented")
@@ -19,7 +22,7 @@ class LabelRemoteDataSourceImpl @Inject constructor(
     }
 
     override suspend fun syncLabel(label: LabelEntity): LabelEntity {
-        TODO("Not yet implemented")
+        return syncApiService.syncLabel(label.toSyncLabelRequest()).data.toData()
     }
 
     override suspend fun deleteLabel(id: String): LabelEntity {
