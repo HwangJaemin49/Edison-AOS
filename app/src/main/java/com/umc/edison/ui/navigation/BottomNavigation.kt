f5f68e1524e1cf1fc7ab7d942dfefdd1a21ab55e
SuHyeon00 <o.ss.u2056@gmail.com>
2025-06-25 01:55:31 +0900

    :arrow_up: Chore(#103): multiplatform-setting, Kotlin 버전 호환되도록 라이브러이 버전 업데이트

--- app/src/main/java/com/umc/edison/ui/navigation/BottomNavigation.kt
+++ app/src/main/java/com/umc/edison/ui/navigation/BottomNavigation.kt
@@ -3,18 +3,17 @@ package com.umc.edison.ui.navigation
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.indication
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.ripple.LocalRippleTheme
-import androidx.compose.material.ripple.RippleAlpha
-import androidx.compose.material.ripple.RippleTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -81,70 +80,63 @@ fun BottomNavigation(
         BottomNavItem.MyPage
     )
 
-    CompositionLocalProvider(LocalRippleTheme provides NoRippleTheme) {
-        NavigationBar(
-            modifier = Modifier
-                .background(color = White000)
-                .border(width = 1.dp, color = Gray100)
-                .padding(horizontal = 8.dp, vertical = 6.dp),
-            containerColor = White000,
-        ) {
-            val navBackStackEntry by navController.currentBackStackEntryAsState()
-            val currentRoute = navBackStackEntry?.destination?.route
+    NavigationBar(
+        modifier = Modifier
+            .background(color = White000)
+            .border(width = 1.dp, color = Gray100)
+            .padding(horizontal = 8.dp, vertical = 6.dp),
+        containerColor = White000,
+    ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
 
-            items.forEach { navItem ->
-                // 현재 라우트가 세부 화면일 경우 상위 라우트로 매핑
-                val isSelected = when (navItem) {
-                    BottomNavItem.MyEdison -> currentRoute?.startsWith(NavRoute.MyEdison.route) == true
-                    BottomNavItem.Space -> currentRoute?.startsWith(NavRoute.Space.route) == true
-                    BottomNavItem.ArtLetter -> currentRoute?.startsWith(NavRoute.ArtLetter.route) == true
-                    BottomNavItem.MyPage -> currentRoute?.startsWith(NavRoute.MyPage.route) == true
-                    BottomNavItem.Bubble -> false
-                }
-
-                NavigationBarItem(
-                    icon = {
-                        Image(
-                            painter = painterResource(id = if (isSelected) navItem.selectedIcon else navItem.icon),
-                            contentDescription = navItem.route
-                        )
-                    },
-                    label = {
-                        if (navItem != BottomNavItem.Bubble) {
-                            Text(
-                                text = LocalContext.current.getString(navItem.title),
-                                color = if (isSelected) Gray800 else Gray400,
-                                style = MaterialTheme.typography.labelSmall
-                            )
-                        }
-                    },
-                    selected = isSelected,
-                    onClick = {
-                        if (navItem == BottomNavItem.Bubble) {
-                            onBubbleClick()
-                        } else {
-                            // navController에 쌓여있는 모든 라우트를 제거하고 해당 라우트로 이동
-                            navController.navigate(navItem.route!!) {
-                                popUpTo(navController.graph.startDestinationId) {
-                                    saveState = false
-                                }
-                                launchSingleTop = true
-                            }
-                        }
-                    },
-                    colors = NavigationBarItemDefaults.colors(
-                        indicatorColor = White000
-                    )
-                )
+        items.forEach { navItem ->
+            // 현재 라우트가 세부 화면일 경우 상위 라우트로 매핑
+            val isSelected = when (navItem) {
+                BottomNavItem.MyEdison -> currentRoute?.startsWith(NavRoute.MyEdison.route) == true
+                BottomNavItem.Space -> currentRoute?.startsWith(NavRoute.Space.route) == true
+                BottomNavItem.ArtLetter -> currentRoute?.startsWith(NavRoute.ArtLetter.route) == true
+                BottomNavItem.MyPage -> currentRoute?.startsWith(NavRoute.MyPage.route) == true
+                BottomNavItem.Bubble -> false
             }
+
+            val interactionSource = remember { MutableInteractionSource() }
+
+            NavigationBarItem(
+                icon = {
+                    Image(
+                        painter = painterResource(id = if (isSelected) navItem.selectedIcon else navItem.icon),
+                        contentDescription = navItem.route
+                    )
+                },
+                label = {
+                    if (navItem != BottomNavItem.Bubble) {
+                        Text(
+                            text = LocalContext.current.getString(navItem.title),
+                            color = if (isSelected) Gray800 else Gray400,
+                            style = MaterialTheme.typography.labelSmall
+                        )
+                    }
+                },
+                selected = isSelected,
+                onClick = {
+                    if (navItem == BottomNavItem.Bubble) {
+                        onBubbleClick()
+                    } else {
+                        // navController에 쌓여있는 모든 라우트를 제거하고 해당 라우트로 이동
+                        navController.navigate(navItem.route!!) {
+                            popUpTo(navController.graph.startDestinationId) {
+                                saveState = false
+                            }
+                            launchSingleTop = true
+                        }
+                    }
+                },
+                colors = NavigationBarItemDefaults.colors(
+                    indicatorColor = White000
+                ),
+                modifier = Modifier.indication(interactionSource, null)
+            )
         }
     }
-}
-
-private object NoRippleTheme : RippleTheme {
-    @Composable
-    override fun defaultColor() = White000
-
-    @Composable
-    override fun rippleAlpha() = RippleAlpha(0F, 0F, 0F, 0F)
 }
